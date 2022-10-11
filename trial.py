3e060ac231c5668e5d09a0a5c4a426c618cbd2ac
theOddster <92583328+theOddster@users.noreply.github.com>
2022-10-11 20:15:48 +0800

    Task ni Jeric | Jeric Abrazaldo

--- trial.py
+++ trial.py
@@ -2,10 +2,10 @@
 """
     "Eto yung  function na ilalagay"
     1. Route Type = (Done) - Kevin
-    2. Gas Price for (PH) - Jeric
+    2. Gas Price for (PH) - Jeric (DONE)
     3. Has Ferry - Joey
     4. Bounding Box - Chloe
-    5. Use Traffic = Gawing aware yung user na hindi traffic based yung system - Jeric
+    5. Use Traffic = Gawing aware yung user na hindi traffic based yung system - Jeric (DONE)
 """
 import urllib.parse
 import requests
@@ -26,14 +26,29 @@ while True:
     if json_status == 0:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=============================================")
+
+        if json_data["route"]["options"]["useTraffic"] == False:
+            print("**********************************************")
+            print("Not accurate traffic info.")
+            print("**********************************************\n")
+        else:
+            print("**********************************************")
+            print("Accurate traffic info.")
+            print("**********************************************\n")
+
         print("Directions from " + (orig) + " to " + (dest))
         print("Trip Duration: " + (json_data["route"]["formattedTime"]))
         print("Kilometers: " +
               str("{:.2f}".format((json_data["route"]["distance"]) * 1.61)))
         print("Fuel Used (Ltr): " +
               str("{:.2f}".format((json_data["route"]["fuelUsed"]) * 3.78)))
+        print("Diesel total price: " +
+              str("{:.2f}".format((json_data["route"]["fuelUsed"]) * 3.78 * 71.700)) + "php")
 
         print("=============================================")
+
+
+
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"]) * 1.61) + " km)"))
         print("=============================================\n")
